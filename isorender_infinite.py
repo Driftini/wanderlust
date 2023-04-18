f82f1ae0a228bd42fc75d9e683f6bb4e636c9ea3
Driftini <yurisilveos741@gmail.com>
2023-04-18 15:11:39 +0200

    Added chunks, there are rendering issues to fix

--- isorender_infinite.py
+++ isorender_infinite.py
@@ -1,117 +1,206 @@
 import pygame
 import sys
-import perlin_noise
+from perlin_noise import PerlinNoise
 from pygame.locals import *
 from utils import *
 
-RESOLUTION = (640, 360)
-SCALE = 2
-SCALED_RESOLUTION = (RESOLUTION[0] * SCALE, RESOLUTION[1] * SCALE)
-TARGET_FRAMERATE = 75
+# Pygame config constants
+PY_RESOLUTION = (640, 360)
+PY_SCALE = 2
+PY_SCALED_RES = (PY_RESOLUTION[0] * PY_SCALE, PY_RESOLUTION[1] * PY_SCALE)
+PY_TARGET_FPS = 75
 
-MAP_WIDTH_HALF = int(25 / 2)
-MAP_ORIGIN_OFFSET = (int(0), int(0))
-
+# Tile rendering constants
 TILE_WIDTH = 8
 TILE_HEIGHT = 8
 TILE_STAGGER = 4
 
+# Chunk constants
+CHUNK_SIZE = 8
+
+# Worldgen constants
+WORLD_ORIGIN = (int(0), int(0)) # x, z
+WORLD_HEIGHT = 50
+WORLD_INIT_RADIUS = 3 # Chunk generation radius on world init
+
+# uh not a constant
+RENDER_OFFSET = [int(PY_RESOLUTION[0] / 2), int(PY_RESOLUTION[1] / 2)]
+
+# Sprites
 SPRITE_TILE = pygame.image.load("tile.png")
 SPRITE_TILE_X = pygame.image.load("tile_axis_x.png")
 SPRITE_TILE_Z = pygame.image.load("tile_axis_z.png")
 
-# uh not a constant
-RENDER_OFFSET = [int(RESOLUTION[0] / 2), int(RESOLUTION[1] / 2)]
-
 # Pygame preconfig
 pygame.init()
 pygame.display.set_caption("template")
-game_window = pygame.display.set_mode(SCALED_RESOLUTION, 0, 32, 0, 1)
-game_surface = pygame.Surface(RESOLUTION)
+game_window = pygame.display.set_mode(PY_SCALED_RES, 0, 32, 0, 1)
+game_surface = pygame.Surface(PY_RESOLUTION)
 pygame.mouse.set_visible(False)
 
 clock = pygame.time.Clock()
 frametime = 0
 
+ALREADY_GEN = set()
 
-class Tilemap:
+class Chunk:
+    # Chunks use chunk coordinates,
+    # spanning from 0 to CHUNK_SIZE on the X and Z axes
     def __init__(self):
-        self.map = {}
+        self.tiles = {}
+        self.tiles_old = None  # it has to be different lol
+        self.cache = pygame.Surface(
+            (TILE_WIDTH * 2 * CHUNK_SIZE, TILE_HEIGHT * WORLD_HEIGHT), flags=SRCALPHA
+            #(1000, 1000), flags=SRCALPHA
+        )
 
-    def set_tile(self, x=0, y=0, z=0, tile=SPRITE_TILE):
-        self.map[f"{x} {y} {z}"] = tile
+    # Convert world coords to chunk coords
+    def convert_coords(self, x, z):
+        chunk_x = x % CHUNK_SIZE
+        chunk_z = z % CHUNK_SIZE
 
-    def generate_cube(self):
-        self.map.clear()
+        return chunk_x, chunk_z
 
-        for z in range(-5, 5):
-            for x in range(-5, 5):
-                for y in range(-5, 5):
-                    self.set_tile(x, y, z)
+    def get_tile(self, x, y, z):
+        cx, cz = self.convert_coords(x, z)
 
-    def generate_axes(self):
-        self.map.clear()
+        return self.tiles[f"{cx} y {cz}"]
 
-        for x in range(1, 10):
-            self.set_tile(x=x, tile=SPRITE_TILE_X)
-        for y in range(0, 10):
-            self.set_tile(y=y)
-        for z in range(1, 10):
-            self.set_tile(z=z, tile=SPRITE_TILE_Z)
+    def set_tile(self, x, y, z, tile=SPRITE_TILE):
+        if (0 <= x <= CHUNK_SIZE) and (0 <= y <= WORLD_HEIGHT) and (0 <= z <= CHUNK_SIZE):
+            cx, cz = self.convert_coords(x, z)
 
-    def generate(self, noise_octaves=.1, noise_seed=None, noise_multiplier=10):
-        self.map.clear()
+            self.tiles[f"{cx} {y} {cz}"] = tile
 
-        noise = perlin_noise.PerlinNoise(
-            octaves=noise_octaves, seed=noise_seed)
+    def render_cache(self):
+        self.cache.fill((0, 0, 0, 10))
 
-        for z in range(-MAP_WIDTH_HALF + MAP_ORIGIN_OFFSET[0], MAP_WIDTH_HALF + MAP_ORIGIN_OFFSET[0]):
-            for x in range(-MAP_WIDTH_HALF + MAP_ORIGIN_OFFSET[1], MAP_WIDTH_HALF + MAP_ORIGIN_OFFSET[1]):
-                y_peak = int(noise([z, x]) * noise_multiplier)
+        for coords in self.tiles.keys():
+            cx, y, cz = coords.split(" ")
+            cx, y, cz = int(cx), int(y), int(cz)
 
-                for actual_y in range(-5, y_peak):
-                    actual_x = x - MAP_ORIGIN_OFFSET[1]
-                    actual_z = z - MAP_ORIGIN_OFFSET[0]
-
-                    self.set_tile(actual_x, actual_y, actual_z)
-
-                # self.map[f"{x} {y} {z}"] = TILE_SPRITE
-
-        return noise.seed
-
-    def draw(self):
-        # tilemap_sorted = sorted(self.map.keys())
-        for coords in self.map.keys():
-            x, y, z = coords.split(" ")
-
-            # Conversion from strings
-            x = int(x)
-            y = int(y)
-            z = int(z)
-
-            tile_type = self.map[coords]
+            tile_type = self.tiles[coords]
 
             tinted = tile_type.copy()
 
-            # TODO moving clouds?????
             # Lower y tiles are shadowed
-            shadow_func = 255 + y * 30
-            shadow = clamp(shadow_func, 0, 255)
+            # shadow_func = 255 + y * 30
+            # shadow = clamp(shadow_func, 0, 255)
 
-            tinted.fill((shadow, shadow, shadow, 255),
-                        special_flags=pygame.BLEND_MULT)
+            # tinted.fill((shadow, shadow, shadow, 255),
+            #             special_flags=pygame.BLEND_MULT)
 
-            draw_tile(x, y, z, tinted)
+            # cx is altered to center the chunk in the surface
+            draw_tile(self.cache, cx + CHUNK_SIZE - 1, y - WORLD_HEIGHT, cz, tinted)
+
+    def get_surf(self):
+        if self.tiles != self.tiles_old:
+            self.render_cache()
+            self.tiles_old = self.tiles
+
+        return self.cache
 
 
-tilemap = Tilemap()
+class World:
+    # The world uses world coordinates,
+    # with infinite extension on X and Z axes, WORLD_HEIGHT on Y axis
+    def __init__(self, gentype="normal", seed=None,
+                 y_multiplier=10, noise_octaves=.1):
+        self.chunks = {}
+
+        match gentype:
+            case "normal":
+                self.noise = PerlinNoise(octaves=noise_octaves, seed=seed)
+                self.y_multiplier = y_multiplier
+                self.seed = self.noise.seed
+
+                for cz in range(-WORLD_INIT_RADIUS, WORLD_INIT_RADIUS):
+                    for cx in range(-WORLD_INIT_RADIUS, 1): #magic number works for some reason
+                        self.generate_chunk(cx, cz)
+            case "axes":
+                self.seed = "Axes Test"
+
+                for x in range(1, 10):
+                    self.set_tile(x=x, tile=SPRITE_TILE_X)
+                for y in range(0, 10):
+                    self.set_tile(y=y)
+                for z in range(1, 10):
+                    self.set_tile(z=z, tile=SPRITE_TILE_Z)
+            case "cube":
+                self.seed = "Cube Test"
+
+                for z in range(-5, 5):
+                    for x in range(-5, 5):
+                        for y in range(-5, 5):
+                            self.set_tile(x, y, z)
+
+    # Get chunk from X and Z world coordinates
+    def find_chunk(self, x, z):
+        chunk_coords = (math.floor(x / CHUNK_SIZE), math.floor(z / CHUNK_SIZE))
+        chunk_key = f"{chunk_coords[0]} {chunk_coords[1]}"
+
+        if chunk_key in self.chunks.keys():
+            return self.chunks[chunk_key]
+        else:
+            return False  # maybe unnecessary
+
+    def get_tile(self, x, y, z):
+        chunk = self.find_chunk(x, z)
+
+        return chunk.get_tile(x, y, z)
+
+    def set_tile(self, x=0, y=0, z=0, tile=SPRITE_TILE):
+        chunk = self.find_chunk(x, z)
+
+        if chunk:
+            chunk.set_tile(x, y, z, tile)
+        # TODO gen chunk if needed
+
+    def generate_chunk(self, cx, cz):
+        if not self.find_chunk(cx, cz):
+            chunk = self.chunks[f"{cx} {cz}"] = Chunk()
+            print(f"generating {cx} {cz}")
+
+            for z in range(0, CHUNK_SIZE):
+                # Corresponding world Z
+                wz = WORLD_ORIGIN[1] + z + cz * CHUNK_SIZE
+                
+                for x in range(0, CHUNK_SIZE):
+                    wx = WORLD_ORIGIN[0] + x + cx * CHUNK_SIZE
+                    
+                    y_peak = int(self.noise([wz, wx]) * self.y_multiplier)
+
+                    #print(f"CHUNK x{cx} z{cz} | COORDS x{wx} y{y_peak} z{wz}")
+
+                    #for actual_y in range(0, y_peak + 5):
+                    for actual_y in range(0, WORLD_HEIGHT):
+                        chunk.set_tile(x, actual_y, z)
+
+    def draw(self, surf):
+        for chunk_coords in self.chunks.keys():
+            cx, cz = chunk_coords.split(" ")
+            cx, cz = int(cx), int(cz)
+
+            c_surf = self.chunks[chunk_coords].get_surf()
+            c_srect = c_surf.get_rect()
+            
+            s = 32
+            
+            draw_pos = (
+                (RENDER_OFFSET[0] + cx * c_srect.width / 2 - cz * c_srect.width / 2),
+                (RENDER_OFFSET[1] + cz * s + cx * s)
+            )
+
+            surf.blit(c_surf, draw_pos)
 
 
-def draw_tile(x, y, z, tile):
-    game_surface.blit(tile, (
-        RENDER_OFFSET[0] + (x * TILE_WIDTH - z * TILE_WIDTH),
-        RENDER_OFFSET[1] + (z * TILE_STAGGER + x *
-                            TILE_STAGGER - y * TILE_HEIGHT)
+world = World()
+
+
+def draw_tile(surf, x, y, z, tile):
+    surf.blit(tile, (
+        (x * TILE_WIDTH - z * TILE_WIDTH),
+        (z * TILE_STAGGER + x * TILE_STAGGER - y * TILE_HEIGHT)
     ))
 
 
@@ -119,9 +208,6 @@ m_left = False
 m_right = False
 m_up = False
 m_down = False
-
-# just here for show in the debug overlay
-n_seed = 0
 
 # Gameloop
 while True:
@@ -143,14 +229,11 @@ while True:
             if event.key == K_s:
                 m_down = True
             if event.key == K_F1:
-                n_seed = tilemap.generate(
-                    noise_octaves=0.1, noise_multiplier=10)
+                world = World("normal")
             if event.key == K_F2:
-                n_seed = 0
-                tilemap.generate_axes()
+                world = World("axes")
             if event.key == K_F3:
-                n_seed = 0
-                tilemap.generate_cube()
+                world = World("cube")
         if event.type == KEYUP:
             if event.key == K_a:
                 m_left = False
@@ -171,11 +254,11 @@ while True:
         RENDER_OFFSET[1] -= 5
 
     # Draw to surface
-    # draw_tilemap(tilemap)
-    tilemap.draw()
+    world.draw(game_surface)
 
     game_window.blit(pygame.transform.scale(
-        game_surface, SCALED_RESOLUTION), (0, 0))
+        game_surface, PY_SCALED_RES), (0, 0)
+    )
 
     # Debug overlay
 
@@ -183,21 +266,21 @@ while True:
     debug_add(f"Frametime: {frametime}ms")
     debug_add("")
 
-    debug_add(f"Tiles: {len(tilemap.map)}")
-    if n_seed > 0:
-        debug_add(f"Map seed: {n_seed}")
-        debug_add(f"Map width: {MAP_WIDTH_HALF * 2} tiles")
+    debug_add(f"Tiles count: TODO")
+    if world.seed:
+        debug_add(f"World seed: {world.seed}")
+        debug_add(f"Chunk count: {len(world.chunks)}")
         debug_add(
-            f"Map origin: Z {MAP_ORIGIN_OFFSET[0]}, X {MAP_ORIGIN_OFFSET[1]}")
+            f"World origin: Z {WORLD_ORIGIN[0]}, X {WORLD_ORIGIN[1]}")
     debug_add(
         f"Rendering offset: {int(RENDER_OFFSET[0])}, {int(RENDER_OFFSET[1])}")
     debug_add("")
 
-    debug_add("F1: Generate random tilemap")
-    debug_add("F2: Generate axes map")
-    debug_add("F3: Generate cube map")
+    debug_add("F1: Generate random world")
+    debug_add("F2: Generate axes world")
+    debug_add("F3: Generate cube world")
 
     # Draw surface to window
     debug_draw()
     pygame.display.update()
-    frametime = clock.tick(TARGET_FRAMERATE)
+    frametime = clock.tick(PY_TARGET_FPS)
