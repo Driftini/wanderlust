829c5b122674f2f992603f55c89bd35a186f5531
Driftini <yurisilveos741@gmail.com>
2023-05-29 17:51:23 +0200

    Forgot to remove Mover from engine.entities

--- engine/entities.py
+++ engine/entities.py
@@ -37,30 +37,3 @@ class Entity:
             self.velocity.update()  # clear velocity
 
             self.cube.topleft = self.pos.xz
-
-
-class Mover(Entity):
-    def __init__(self, pos=[0.0, 0.0, 0.0]):
-        super().__init__(pos)
-
-        self.size = [TILE_WIDTH, TILE_HEIGHT, TILE_WIDTH]  # tile-sized
-        self.speed = .3
-
-    def update(self, dt=0):
-        super().update(dt)
-
-        if get_control("left"):
-            self.velocity.x -= self.speed
-            self.velocity.z += self.speed
-        if get_control("right"):
-            self.velocity.x += self.speed
-            self.velocity.z -= self.speed
-        if get_control("up"):
-            self.velocity.z -= self.speed
-            self.velocity.x -= self.speed
-        if get_control("down"):
-            self.velocity.z += self.speed
-            self.velocity.x += self.speed
-
-        debug_add(f"entity velocity: {self.velocity.xyz}")
-        debug_add(f"entity pos: {self.pos.xyz}")
